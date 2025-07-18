090eaa86778d51ef8d6a6dc7fa37912a013ac1d6
Ludwik C. Siadlak <ludwikc@users.noreply.github.com>
2025-07-18 11:57:00 +0200

    Refactored index.html to index.php and /partials

---
+++ assets/js/lifehackerzy-email-to-easycart.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+            $('#submitButton').on('click', function() {
+                // Capture the email input value
+                var email = encodeURIComponent($('#emailInput').val());
+                
+                // Construct the URL with the email parameter
+                var baseUrl = 'https://app.easycart.pl/checkout/siadlak/lifehackerzy';
+                var email = encodeURIComponent($('#emailInput').val());
+                var url = baseUrl + '?email=' + email;
+                
+                // Redirect to the constructed URL
+                window.location.href = url;
+            });
+        });
---
+++ index-refactored.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <?php include 'partials/head.html'; ?>
+</head>
+
+<body>
+      <?php include 'partials/after-body-open.html'; ?>
+    <div class="page-wrapper">
+        
+        
+        <?php include 'partials/header.html'; ?>
+        
+        <!-- Main Content Sections -->
+        <?php include 'sections/hero-banner.html'; ?>
+        <?php include 'sections/solution-explanation.html'; ?>
+        <?php include 'sections/features-carousel.html'; ?>
+        <?php include 'sections/livechat.html'; ?>
+        <?php include 'sections/statistics.html'; ?>
+        <?php include 'sections/conversation.html'; ?>
+        <?php include 'sections/protipy-tiles.html'; ?>
+        <?php include 'sections/toolbox.html'; ?>
+        <?php include 'sections/testimonials.html'; ?>
+        <?php include 'sections/tools-showcase.html'; ?>
+        <?php include 'sections/pricing.html'; ?>
+        <?php include 'sections/team.html'; ?>
+        <?php include 'sections/course-modules.html'; ?>
+        <?php include 'sections/workshops.html'; ?>
+        <?php include 'sections/faq.html'; ?>
+        
+        <?php include 'partials/footer.html'; ?>
+        
+        <!-- Scroll To Top -->
+        <div class="scroll-to-top scroll-to-target" data-target="html">
+            <span class="fa fa-arrow-up"></span>
+        </div>
+        
+    </div>
+    
+    <?php include 'partials/scripts.html'; ?>
+    
+</body>
+</html>
